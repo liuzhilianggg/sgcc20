7443e7f299120627fb2f7fe1e63876b4a69ad56e
zs <zs@bjpowernode.com>
2020-01-14 15:01:40 +0800

    springcloud初建

---
+++ src/main/java/com/sgcc/springcloud/User.java
@@ -0,0 +1,15 @@
+package com.sgcc.springcloud;
+
+/**
+ * className:User
+ * package:com.sgcc.springcloud
+ * Discription:
+ *
+ * @date:2020/1/14 14:53
+ * @ayther:liushushu
+ */
+public class User {
+
+    private String Username;
+
+}
